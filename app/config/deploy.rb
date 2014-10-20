ec95420da3773712d00eb17c86511841d000fe09
lowki93 <lowkidu93@live.fr>
2014-10-20 20:58:49 +0000

    add config for deploy with capifony

---
+++ app/config/deploy.rb
@@ -0,0 +1,30 @@
+set :stage_dir, 'app/config/deploy'
+
+require 'capistrano/ext/multistage'
+
+set :stages, %w(prod demo)
+
+set :application, "Portfolio Laure"
+set :app_path,    "app"
+set :user, "lowki"
+
+set :repository,  ".git"
+set :scm,         :git
+
+set :deploy_via, :rsync_with_remote_cache
+
+set :assets_symlink, true
+set :model_manager, "doctrine"
+set :use_sudo, false
+set :keep_releases,  3
+
+set :shared_files, ["app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :use_composer, true
+set :dump_assetic_assets, true
+set :interactive_mode, true
+
+set :php_bin, "php -d apc.enable_cli=0"
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::INFO
---
+++ app/config/deploy/prod.rb
@@ -0,0 +1,8 @@
+set :domain, "178.32.221.32"
+set :symfony_env_prod, "prod"
+
+set :deploy_to, "/home/lowki/instances/laure"
+
+role :web, domain
+role :app, domain
+role :db, domain, :primary => true
