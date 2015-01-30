94b8bd6c1169bd5be65722bf4b0b80bb6ab2eddc
lowki93 <lowkidu93@live.fr>
2015-01-30 14:20:21 +0000

    add enity aboutFeeling

---
+++ app/DoctrineMigrations/Version20150130141954.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150130141954 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE pfl_AboutFeeling_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE pfl_AboutFeeling (id INT NOT NULL, name VARCHAR(255) NOT NULL, percentage INT NOT NULL, color VARCHAR(7) NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP SEQUENCE pfl_AboutFeeling_id_seq CASCADE');
+        $this->addSql('DROP TABLE pfl_AboutFeeling');
+    }
+}
---
+++ src/Portfolio/LaureBundle/Entity/AboutFeeling.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Portfolio\LaureBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * AboutFeeling
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class AboutFeeling
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="percentage", type="integer")
+     */
+    private $percentage;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="color", type="string", length=7)
+     */
+    private $color;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return AboutFeeling
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set percentage
+     *
+     * @param integer $percentage
+     * @return AboutFeeling
+     */
+    public function setPercentage($percentage)
+    {
+        $this->percentage = $percentage;
+
+        return $this;
+    }
+
+    /**
+     * Get percentage
+     *
+     * @return integer 
+     */
+    public function getPercentage()
+    {
+        return $this->percentage;
+    }
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     * @return AboutFeeling
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get color
+     *
+     * @return string 
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+}
