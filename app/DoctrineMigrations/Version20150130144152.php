46c84eeb5067415e8fbc2b5af20bdff031420bf9
lowki93 <lowkidu93@live.fr>
2015-01-30 14:48:51 +0000

    add timestampable for AboutFeeling

---
+++ app/DoctrineMigrations/Version20150130144152.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150130144152 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE pfl_aboutfeeling ADD createdAt TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+        $this->addSql('ALTER TABLE pfl_aboutfeeling ADD updatedAt TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE pfl_AboutFeeling DROP createdAt');
+        $this->addSql('ALTER TABLE pfl_AboutFeeling DROP updatedAt');
+    }
+}
