e1aa7d32073f5d3db6e5874225e1beb74bddc47e
lowki93 <lowkidu93@live.fr>
2015-02-18 10:42:14 +0000

    add new entity

---
+++ src/Portfolio/LaureBundle/Entity/WorkExperienceNb.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Portfolio\LaureBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * WorkExperienceNb
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class WorkExperienceNb
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="number", type="integer")
+     */
+    private $number;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set number
+     *
+     * @param integer $number
+     * @return WorkExperienceNb
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return integer 
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+}
